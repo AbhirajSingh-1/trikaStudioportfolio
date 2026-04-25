d47cc83d3264ad21eb818f743fa46c8647a16684
abhiraj <AbhirajSingh-1@users.noreply.github.com>
2026-04-25 14:07:21 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home/Home';
 import AIAdvertising from './pages/AIAdvertising/AIAdvertising';
-import Visualization from './pages/3DVisualization/Visualization';
+import Visualization from './pages/3Dvisualization/Visualization';
 import DigitalMarketing from './pages/DigitalMarketing/DigitalMarketing';
 import About from './pages/About/About';
 import Contact from './pages/Contact/Contact';
